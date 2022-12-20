0722383f3b8d0c3075510d27d831ca7f16be857f
lunox-code <lunox.code@gmail.com>
2022-12-20 11:02:58 -0600

    AOS bug fixed

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,9 @@ config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatic
 
 import { useEffect } from "react";
 import AOS from "aos";
-
 import "aos/dist/aos.css";
 
 function MyApp({ Component, pageProps }) {
-  <script src="https://unpkg.com/aos@next/dist/aos.js"></script>;
   useEffect(() => {
     AOS.init();
     AOS.refresh();
